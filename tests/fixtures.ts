34abc2d9234ebc88edd01a0424a5390b0e76ae4a
Serdal Özçetin <ozcetinserdal@gmail.com>
2024-08-19 18:48:55 +0300

    API

---
+++ tests/fixtures.ts
@@ -0,0 +1,31 @@
+// fixtures.ts
+import {
+  test as base,
+  expect,
+  request,
+  APIRequestContext,
+} from "@playwright/test";
+
+type TestFixtures = {
+  apiContext: APIRequestContext;
+  totalElements: number;
+};
+
+const test = base.extend<TestFixtures>({
+  apiContext: async ({}, use) => {
+    const context = await request.newContext({
+      baseURL: "https://jsonplaceholder.typicode.com",
+    });
+    await use(context);
+    await context.dispose();
+  },
+});
+
+const payLoad = {
+  userId: 10,
+  id: 101,
+  title: "serdal",
+  body: "I am trying to create new one",
+};
+
+export { test, expect, request, APIRequestContext, payLoad };
---
+++ tests/symphonySolutionsAPI.spec.ts
@@ -0,0 +1,140 @@
+import { APIResponse } from "@playwright/test";
+import { test, expect, payLoad } from "./fixtures";
+
+let totalElements: number;
+let createdPostId: number;
+
+test.describe("Symphony Solutions-API test", async () => {
+  test.describe.configure({ mode: "serial" });
+  /*----------------------------------------------------------------------------------------------------------------------------------*/
+
+  test("Read Total Number of Posts and Store in a Variable", async ({
+    apiContext,
+  }) => {
+    let responseJson;
+    let lastElement: any;
+
+    await test.step("Send a GET request to /posts.", async () => {
+      const response = await apiContext.get("/posts");
+      expect(response.ok()).toBeTruthy();
+      responseJson = await response.json();
+    });
+
+    await test.step("Retrieve the total number of posts from the response body", async () => {
+      totalElements = responseJson.length;
+      console.log("Total number of posts:", totalElements);
+    });
+
+    await test.step("Store the total number of posts in a variable for future reference", async () => {
+      lastElement = responseJson[totalElements - 1];
+      console.log("Last element ID:", lastElement.id);
+      expect(lastElement.id).toBe(totalElements);
+    });
+  });
+  /*----------------------------------------------------------------------------------------------------------------------------------*/
+
+  test("Create a New Post and Store its ID", async ({ apiContext }) => {
+    let responseJson;
+    await test.step("Send a 'POST' request to /posts with a new post object containing relevant data", async () => {
+      const response = await apiContext.post("/posts", {
+        data: payLoad,
+      });
+      expect(response.ok()).toBeTruthy();
+      responseJson = await response.json();
+    });
+
+    await test.step("Extract the ID of the newly created post from the response body and store it in a variable for future reference", async () => {
+      createdPostId = responseJson.id;
+      console.log("New element ID:", createdPostId);
+      console.log("Total elements before creation:", totalElements);
+    });
+  });
+  /*----------------------------------------------------------------------------------------------------------------------------------*/
+
+  test("Get Only the Created Post by ID", async ({ apiContext }) => {
+    console.log(
+      "The ID of the post created in the previous test:",
+      createdPostId
+    );
+    let response: APIResponse;
+
+    await test.step("Send a 'GET' request to /posts/{id} endpoint, replacing {id} with the ID of the created post", async () => {
+      response = await apiContext.get(`/posts/${createdPostId}`);
+    });
+
+    await test.step("Retrieve and validate the details of the created post from the response body", async () => {
+      const responseJson = await response.json();
+      console.log(" Created item status code: ", response.status());
+      //expect(response.ok()).toBeTruthy();
+      //expect(responseJson.id).toBe(createdPostId);
+    });
+  });
+  /*----------------------------------------------------------------------------------------------------------------------------------*/
+  test("Replace Some Field in the Created Post with PATCH", async ({
+    apiContext,
+  }) => {
+    let textForPatch: string = "***replaced field***";
+    let responseJson;
+    let response: APIResponse;
+
+    await test.step("Send a PATCH request to /posts/{id} with updated field(s) of the post, replacing {id} with the ID of the created post", async () => {
+      response = await apiContext.patch(`/posts/${createdPostId}`, {
+        data: {
+          userId: 10,
+          id: 101,
+          title: "Serdal",
+          body: textForPatch,
+        },
+      });
+    });
+
+    await test.step("Confirm the successful update of the post by sending a GET request to /posts/{id} and verifying the changes", async () => {
+      console.log("Updated item status code: ", response.status());
+      responseJson = await response.json();
+      expect(response.ok()).toBeTruthy();
+      const patchedField: string = responseJson.body;
+      console.log(patchedField);
+      expect(textForPatch).toBe(patchedField);
+    });
+  });
+  /*----------------------------------------------------------------------------------------------------------------------------------*/
+  test("Delete the Created Post by ID", async ({ apiContext }) => {
+    await test.step("Send a DELETE request to /posts/{id} to delete the post, replacing {id} with the ID of the created post", async () => {
+      const response = await apiContext.delete(`/posts/${createdPostId}`);
+      console.log("Deleted item status code: ", response.status());
+    });
+
+    await test.step("Verify that the post has been successfully deleted by attempting to retrieve it using a GET request to /posts/{id} and ensuring a '404' status code is returned", async () => {
+      const response = await apiContext.get(`/posts/${createdPostId}`);
+      const responseJson = await response.json();
+      console.log(
+        "After deleting, created item satatus code: ",
+        response.status()
+      );
+      expect(404).toBe(response.status());
+    });
+  });
+
+  /*----------------------------------------------------------------------------------------------------------------------------------*/
+  test("Check the Number of Posts to Ensure Integrity", async ({
+    apiContext,
+  }) => {
+    let responseJson;
+    let lastTotalElements: number;
+    await test.step("Send a 'GET' request to /posts", async () => {
+      const response = await apiContext.get("/posts");
+      responseJson = await response.json();
+      expect(response.ok()).toBeTruthy();
+      console.log("The last status code: ", response.status());
+    });
+
+    await test.step("Retrieve the current total number of posts from the response body", async () => {
+      lastTotalElements = responseJson.length;
+      console.log("The last total number of posts:", lastTotalElements);
+    });
+
+    await test.step("Compare the current total number of posts with the initial total number obtained in step 1 to ensure integrity", async () => {
+      expect(lastTotalElements).toBe(totalElements);
+    });
+  });
+});
